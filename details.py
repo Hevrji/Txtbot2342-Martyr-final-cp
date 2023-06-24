78544215b20cb9317118aed7c3f6cf38e604c0e4
Martyy2003 <131404553+Martyy2003@users.noreply.github.com>
2023-06-24 21:28:35 +0530

    Update details.py

--- details.py
+++ details.py
@@ -1,4 +1,4 @@
 api_id = 12935455
 api_hash = "46efd25a28173b86ca43fc1876915149"
-bot_token = "6138844016:AAGSay4MdhdQxUDHZ-U5nOxOF0YDlF7nHjg"
+bot_token = "6150009394:AAH-AewKKW_9n8a9qSDOl9rxbMo7dErrKCE"
 sudo_groups = -1001648433190
