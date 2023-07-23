d108803b84b1c440ef76dd73e171b6abacfbeb69
Prakash-Baraiya <133520032+Prakash-Baraiya@users.noreply.github.com>
2023-07-23 11:35:28 +0530

    Update details.py

--- details.py
+++ details.py
@@ -1,4 +1,4 @@
-api_id = 12935455
-api_hash = "46efd25a28173b86ca43fc1876915149"
-bot_token = "6150009394:AAH-AewKKW_9n8a9qSDOl9rxbMo7dErrKCE"
-sudo_groups = -1001648433190
+api_id = 20717621
+api_hash = "a68af155e7f0c79b1baab1f185f4181d"
+bot_token = "6573278327:AAGI7ukXgOVTFkKrITQ6teW7eAzrSmPulAI"
+sudo_groups = -1001663303433
