b9d829acc59eeb937cf71dc22c57e83902d6d16b
Hevrji <116875997+Hevrji@users.noreply.github.com>
2023-12-01 19:45:46 +0530

    Update details.py

--- details.py
+++ details.py
@@ -1,4 +1,4 @@
 api_id = 20717621
 api_hash = "a68af155e7f0c79b1baab1f185f4181d"
-bot_token = "6573278327:AAGI7ukXgOVTFkKrITQ6teW7eAzrSmPulAI"
+bot_token = "6802599697:AAFp_3GaAVt9FM4KpVn0uEjnJIrKU49cHmo"
 sudo_groups = -1001663303433
